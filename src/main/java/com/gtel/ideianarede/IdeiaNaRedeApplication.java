6190c0773369f54b4a0f8600e5a9bc64243cd32f
Bruno Lucio <brssousa@gmail.com>
2021-04-11 10:33:31 -0300

    Inicializando Projeto Ideia na Rede

---
+++ src/main/java/com/gtel/ideianarede/IdeiaNaRedeApplication.java
@@ -0,0 +1,13 @@
+package com.gtel.ideianarede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeiaNaRedeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeiaNaRedeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtel/ideianarede/IdeiaNaRedeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtel.ideianarede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeiaNaRedeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
